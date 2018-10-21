14e13e9887ae9cc2bcd38418a130ec9ab1653c6e
freeloginpls <44008190+freeloginpls@users.noreply.github.com>
2018-10-22 00:00:48 +0200

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -6,19 +6,28 @@ Created on Sun Oct 21 13:22:15 2018
 """
 
 #zad1
-from miszcz import funkcja
+#podejscie 1
+from miszcz.funkcja import ciekawafunkcjawersjapython
 
 print(ciekawafunkcjawersjapython(665))    
 print(ciekawafunkcjawersjapython(-2))
 print(ciekawafunkcjawersjapython(0))
 
+#podejscie 2
+import miszcz.funkcja
+print(miszcz.funkcja.ciekawafunkcjawersjapython(665))    
+
+
+#podejscie 3
+import miszcz.funkcja as debil
+print(debil.ciekawafunkcjawersjapython(3))
 #zad2
 
 def double(x):
-    if isinstance(x, (int,float)):
-        return((x,x*2))
-    if isinstance(x,list):
-        return((x*2,[i * 2 for i in x]))
+    if isinstance (x, (int,float)):
+        return x, x*2
+    if isinstance (x,list):
+        return x*2, [i * 2 for i in x]
     
 
 
